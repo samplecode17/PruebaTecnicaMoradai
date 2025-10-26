91ed30cd23750aacf3da82fcad8a7020062731bf
samplecode17 <ibrahim_mou17@hotmail.com>
2025-10-26 18:43:32 +0100

    feat: - create and inject router - create empty views

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,16 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store';
+import router from './router';
 
 //app creation
 const app = createApp(App);
 
 //inject vuex global state config
 app.use(store);
+//inject router 
+app.use(router);
+
+app.mount("#app");
+
 
