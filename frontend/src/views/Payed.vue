19d27ea52626c92313f5c41774cced49ce889d2b
samplecode17 <ibrahim_mou17@hotmail.com>
2025-10-26 22:34:03 +0100

    feat: added Payment correct view

--- frontend/src/views/Payed.vue
+++ frontend/src/views/Payed.vue
@@ -1,5 +1,51 @@
 <template>
-
+  <div class="success-container">
+    <h1 class="success-message">
+      🎉 pago completado 🎉
+    </h1>
+    <button @click="goToHome" >volver al inicio</button>
+  </div>
 </template>
 <script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const goToHome = () => {
+  router.push('/') // navigate to home
+}
+
+
 </script>
+<style>
+.success-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* ocupies all the screen*/
+  text-align: center;
+}
+
+.success-message {
+  font-size: 3rem; /* Great text */
+  font-weight: bold;
+  color: #2c3e50; 
+  margin-bottom: 30px;
+}
+
+button {
+  padding: 12px 30px;
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: white;
+  background-color: #064581;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+button:hover {
+  background-color: #006afdff;
+}
+</style>
